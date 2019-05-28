eb30f2173e38d2c0757a3b34a18ffd78b56820b8
Bieelff <gabriel.alvesferreira@gmail.com>
2019-05-28 15:44:00 -0300

    aula 26

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,61 +1,51 @@
-console.log("App.js is running!")
-
-const app = {
-    title: "Indecision App",
-    subtitle: "Put your life in the gands of a computer",
-    options: []
-};
-
-const onFormSubmit = (e) => {
-    e.preventDefault();
-    const option = e.target.elements.option.value;
-
-    if (option) {
-        app.options.push(option);
-        e.target.elements.option.value = "";
-        render();
-    };
-};
-
-const onRemoveAll = (e) => {
-    app.options = []
-    render();
-};
-
-
-const onMakeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length);
-    const option = app.options[randomNum];
-    alert(option);
-};
-
-const appRoot = document.getElementById("app");
-
-const render = () => {
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-            <button disabled={app.options.length === 0} onClick={onMakeDecision}> What should I do?</button>
-            <button onClick={onRemoveAll}>Remove All</button>
-            {/*
-                numbers.map((number) => {
-                    return <p key={number}>Number: {number}</p>
-                })
-            */}
-            <ol>
-                {
-                    app.options.map((option) => <li key={option}>{option}</li>)
-                }
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option" />
-                <button>Add Option</button>
-            </form>
-        </div>
-    );
-    ReactDOM.render(template, appRoot);
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a copmuter</h2>
+            </div>
+        );
+    }
 }
 
-render();
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Options Component here</p>
+            </div>
+        )
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>AddOption component here</p>
+            </div>
+        )
+    }
+}
+
+const jsx = (
+    <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"))
