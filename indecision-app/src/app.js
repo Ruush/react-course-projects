aa6b13ad3508ba97930e36f4ea43715c144b22b2
Bieelff <gabriel.alvesferreira@gmail.com>
2019-05-28 11:51:38 -0300

    aula 19

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -24,6 +24,7 @@ const onRemoveAll = (e) => {
 
 const appRoot = document.getElementById("app");
 
+
 const render = () => {
     const template = (
         <div>
@@ -32,9 +33,15 @@ const render = () => {
             <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
             <p>{app.options.length}</p>
             <button onClick={onRemoveAll}>Remove All</button>
+            {/*
+                numbers.map((number) => {
+                    return <p key={number}>Number: {number}</p>
+                })
+            */}
             <ol>
-                <li>Item one</li>
-                <li>Item two</li>
+                {
+                    app.options.map((option) => <li key={option}>{option}</li>)
+                }
             </ol>
             <form onSubmit={onFormSubmit}>
                 <input type="text" name="option" />
