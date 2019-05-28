0806466813856cda6397b60984d27c181e038c71
Bieelff <gabriel.alvesferreira@gmail.com>
2019-05-28 15:29:17 -0300

    aula 25

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -5,12 +5,47 @@ class Person {
         this.age = age;
     }
     getGretting() {
-        return `Hi. I am ${this.name} and i have ${this.age} years old! `
+        return `Hi. I am ${this.name}!"`
+    }
+    getDescription() {
+        return `${this.name} is ${this.age} year(s) old.`
     }
 };
 
-const me = new Person("jose", 35);
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}`;
+        }
+        return description
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, location){
+        super(name, age);
+        this.location = location;
+    }
+    getGretting(){
+        let gretting = super.getGretting();
+        if(this.location){
+            gretting += ` I'm visiting from ${this.location}`
+        }
+        return gretting
+    }
+}
+
+const me = new Traveler("Jose Ruela", 35, "Osasco");
 console.log(me.getGretting());
 
-const other = new Person();
+const other = new Traveler();
 console.log(other.getGretting());
