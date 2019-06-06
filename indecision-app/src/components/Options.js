8ae87fc55d001937107a0c7ddf5afa250c3d290f
Bieelff <gabriel.alvesferreira@gmail.com>
2019-06-06 13:08:31 -0300

    aula 55

---
+++ indecision-app/src/components/Options.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Option from "./Option.js"
+
+const Options = (props) => {
+    return (
+        <div>
+            <button
+                onClick={props.handleDeleteOptions}
+                disabled={!props.hasOptions}
+            >
+                Remove All
+            </button>
+            {props.options.length === 0 && <p>Please add an Option to get started!</p>}
+            {
+                props.options.map((option) => (
+                    <Option
+                        key={option}
+                        optionText={option}
+                        handleDeleteOption={props.handleDeleteOption}
+                    />
+                ))
+            }
+        </div>
+    )
+}
+
+export default Options
