5c5d0cb5a532936957d233b726a61eabb555b4a1
Ruush <gabriel.alvesferreira@gmail.com>
2019-07-25 16:44:00 -0300

    aula 122

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -26,7 +26,7 @@ export default class ExpenseForm extends React.Component {
         const note = e.target.value;
         this.setState(() => ({ note }));
     };
-    onAmountChange = e => {
+    onAmountChange = (e) => {
         const amount = e.target.value;
 
         if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
--- expensify-app/src/tests/components/ExpenseForm.test.js
+++ expensify-app/src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,54 @@ test("should render ExpenseForm with expense data", () => {
     const wrapper = shallow(<ExpenseForm expense={expensesTest[0]} />);
     expect(wrapper).toMatchSnapshot();
 });
+
+test("should render error for invalid form submission", () => {
+    const wrapper = shallow(<ExpenseForm />);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find("form").simulate("submit", {
+        preventDefault: () => { }
+    });
+    expect(wrapper.state("error").length).toBeGreaterThan(0);
+    expect(wrapper).toMatchSnapshot();
+})
+
+test("should set description on input change", () => {
+    const value = "New Description";
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find("input").at(0).simulate("change", {
+        target: { value }
+    });
+    expect(wrapper.state("description")).toBe(value);
+    expect(wrapper).toMatchSnapshot();
+})
+
+test("should set note on input change", () => {
+    const value = "New Note";
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find("textarea").simulate("change", {
+        target: { value }
+    });
+    expect(wrapper.state("note")).toBe(value);
+    expect(wrapper).toMatchSnapshot();
+})
+
+
+test("should set amount on input change", () => {
+    const value = "23.50";
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find("input").at(1).simulate("change", {
+        target: { value }
+    })
+    expect(wrapper.state("amount")).toBe(value);
+    expect(wrapper).toMatchSnapshot();
+})
+
+test("should not set amount on input change", () => {
+    const value = "12.222";
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find("input").at(1).simulate("change", {
+        target: { value }
+    });
+    expect(wrapper.state("amount")).toBe("");
+    expect(wrapper).toMatchSnapshot();
+})
